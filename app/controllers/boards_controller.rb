98dffb3c10888e472260af8616acf9cbee931b35
daisuke sasaki <gerund25495641@gmail.com>
2021-01-09 14:16:37 +0900

    flash message作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,8 @@
 class BoardsController < ApplicationController
    before_action :set_target_board, only: %i[show edit update destroy]
+ 
    def index
-      @boards = Board.page(params[:page])
+     @boards = Board.page(params[:page])
    end
  
    def new
@@ -10,33 +11,33 @@ class BoardsController < ApplicationController
  
    def create
      board = Board.create(board_params)
+     flash[:notice] = "「#{board.title}」の掲示板を作成しました"
      redirect_to board
    end
-
+ 
    def show
-      @board = Board.find(params[:id])
    end
-
+ 
    def edit
-       @board = Board.find(params[:id])
    end
-
+ 
    def update
-      board = Board.find(params[:id])
-      board.update(board_params)
-  
-      redirect_to board
+     @board.update(board_params)
+     redirect_to @board
    end
-
-   def destroy 
-      board = Board.find(params[:id])
-      board.delete
-      redirect_to boards_path
+ 
+   def destroy
+     @board.delete
+     redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
    end
-   
+ 
    private
  
    def board_params
      params.require(:board).permit(:name, :title, :body)
+   end
+ 
+   def set_target_board
+     @board = Board.find(params[:id])
    end
  end
