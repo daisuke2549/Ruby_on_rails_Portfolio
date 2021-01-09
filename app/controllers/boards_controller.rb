ff9611a51b0cf2e4c6a8c3d485b56cd444dfea2d
daisuke sasaki <gerund25495641@gmail.com>
2021-01-09 13:22:08 +0900

    show,edit機能追加済み

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+   before_action :set_target_board, only: %i[show edit update destroy]
    def index
      @boards = Board.all
    end
@@ -8,13 +9,31 @@ class BoardsController < ApplicationController
    end
  
    def create
-     Board.create(board_params)
+     board = Board.create(board_params)
+     redirect_to board
    end
 
    def show
       @board = Board.find(params[:id])
    end
- 
+
+   def edit
+       @board = Board.find(params[:id])
+   end
+
+   def update
+      board = Board.find(params[:id])
+      board.update(board_params)
+  
+      redirect_to board
+   end
+
+   def destroy 
+      board = Board.find(params[:id])
+      board.delete
+      redirect_to boards_path
+   end
+   
    private
  
    def board_params
