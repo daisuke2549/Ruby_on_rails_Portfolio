5bbf57091bc6be2c8c910c2478411cebe5a9055d
daisuke sasaki <gerund25495641@gmail.com>
2021-01-11 17:38:14 +0900

    add authentication routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
